03b24959fb84f6e4b561c8a05e91dd9fbb0b8938
Ryan Timmons <ryan.timmons@mongodb.com>
2018-07-18 13:11:41 -0400

    inline handler functions

--- app.js
+++ app.js
@@ -19,43 +19,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static('ui'))
 
-// Handlers
-
-const header = (req, res) => {
-  res.header('Content-Type', 'application/json')
-  return Promise.resolve();
-};
-
-var getStateHandler = function(req, res){
-  return header(req, res)
-    .then(model.getStateJson)
-    .then(s => res.send(s));
-};
-
-var setStateHandler = function(req, res) {
-  return header(req, res)
-    .then(() => model.setState(req.query.Question, req.query.Name, req.query.Vote))
-    .then(s => res.send(s));
-};
-
-var kickHandler = function(req, res) {
-  return header(req, res)
-    .then(() => model.kick(req.query.Name))
-    .then(s => res.send(s));
-};
-
-var resetHandler = function(req, res) {
-  return header(req, res)
-    .then(model.reset)
-    .then(s => res.send(s));
-};
-
-var clearHandler = function(req, res) {
-  return header(req, res)
-    .then(model.clear)
-    .then(s => res.send(s));
-};
-
 // Routing
 
 /*
@@ -82,12 +45,36 @@ kick:
 http://localhost:3000/kick.json?Name=Abdul
 */
 
-// these url routes suck
-app.get('/state.json',  getStateHandler);
-app.get('/set.json',    setStateHandler);
-app.get('/kick.json',   kickHandler);
-app.get('/reset.json',  resetHandler);
-app.get('/clear.json',  clearHandler);
+const header = (req, res) => {
+  res.header('Content-Type', 'application/json')
+  return Promise.resolve();
+};
+
+app.get('/state.json', function(req, res){
+  return header(req, res)
+    .then(model.getStateJson)
+    .then(s => res.send(s));
+});
+app.get('/set.json', function(req, res) {
+  return header(req, res)
+    .then(() => model.setState(req.query.Question, req.query.Name, req.query.Vote))
+    .then(s => res.send(s));
+});
+app.get('/kick.json', function(req, res) {
+  return header(req, res)
+    .then(() => model.kick(req.query.Name))
+    .then(s => res.send(s));
+});
+app.get('/reset.json', function(req, res) {
+  return header(req, res)
+    .then(model.reset)
+    .then(s => res.send(s));
+});
+app.get('/clear.json', function(req, res) {
+  return header(req, res)
+    .then(model.clear)
+    .then(s => res.send(s));
+});
 
 
 // 💪
