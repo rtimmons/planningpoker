fd99608feb3033395df7080ffba99d4a941ebd70
Ryan Timmons <ryan.timmons@mongodb.com>
2018-07-18 15:55:14 -0400

    limit amount of data we keep around

--- model.js
+++ model.js
@@ -8,6 +8,15 @@ This is about at its limit for complexity without some refactoring
 and/or automated testing!
 */
 
+/*
+ 20 voters max at (10 + 50)=60 chars per => max of 20*60=1200 bytes-ish + 100 for the question
+ => we can consume on the order of 2kb of memory :)
+*/
+const QUESTION_LIMIT = 100;
+const MAX_VOTERS     = 20;
+const NAME_LIMIT     = 50;
+const VOTE_LIMIT     = 10;
+
 
 var timestamp = function() { return new Date().getTime(); };
 
@@ -22,8 +31,14 @@ const startState = {
 };
 var state = deepcopy(startState);
 
+var shorten = function(str, max) {
+  return (_.isUndefined(str) || !_.isString(str)) ? str : (str.length >= max ? str.substring(0,max-1) : str);
+}
 
 var getStateJson = function() {
+  if ((state.Voters || []).length >= MAX_VOTERS) {
+    return reset();
+  }
   return Promise.resolve(JSON.stringify(state));
 };
 
@@ -32,8 +47,11 @@ var setState = function(question, vname, vote) {
 
   // can set question even if other errors
   state.Question = question || state.Question;
+  state.Question = shorten(state.Question, QUESTION_LIMIT);
 
   if (!(vname || !_.isUndefined(vote))) { return; }
+
+  vname = shorten(vname, NAME_LIMIT);
 
   voter = _.find(state.Voters, (v) => v.Name == vname);
   existing = ! _.isUndefined(voter);
@@ -45,7 +63,11 @@ var setState = function(question, vname, vote) {
   voter = voter || {};
 
   voter.Name = vname || voter.Name;
-  voter.Vote = vote  || voter.Vote;
+  voter.Name = shorten(voter.Name, NAME_LIMIT);
+
+  voter.Vote = vote || voter.Vote;
+  voter.Vote = shorten(voter.Vote, VOTE_LIMIT);
+
   voter.LastVoteTS = timestamp();
 
   if (!existing) {
@@ -56,6 +78,7 @@ var setState = function(question, vname, vote) {
 };
 
 var kick = function(vname) {
+  vname = shorten(vname, NAME_LIMIT);
   state.Voters = _.reject(state.Voters, (v) => v.Name == vname);
 
   return getStateJson();
